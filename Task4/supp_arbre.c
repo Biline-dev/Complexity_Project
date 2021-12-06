3a1cc8550a6ff2bfd4c71f95051d35db44a525bc
Nasroo <63188489+Nasroo@users.noreply.github.com>
2021-12-06 23:08:14 +0100

    Update supp_arbre.c

--- Task4/supp_arbre.c
+++ Task4/supp_arbre.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include<string.h>
+
 
 #define BILLION 1000000000.0
 
@@ -55,17 +57,19 @@ Node* insert(Node *tee, int x){
         else    
             tee->right = insert(tee->right, x);
 }
-void fillTree(Node *tee, char text[20]){
+void fillTree(Node *tee, char txt[20], int size){
     int data;
     FILE *fp;
     char s[10];
-    fp = fopen(s, "r");
-    for(int i=0; i<10000; i++){
-        fgets(text, 20, fp);
+    int i;
+    fp = fopen(txt, "r");
+    for(i=0; i<size; i++){
+        fgets(s, 20, fp);
         //printf("this is it : %s", s);
         data = atoi(s);
         insert(tee, data);
     }
+    fclose(fp);
 }
 
 void printTree(Node *tee){
@@ -128,31 +132,94 @@ Node *deleteNode(Node *tee, int x){
 //struct timespec start, end;
 int main()
 {
+    FILE *f1, *f2, *f3;
+    char s[20];
+    double time_spent;
+    char step[6];
+    struct timespec start, end;
+    f1=fopen("resultPire.txt", "r+");
+    f2=fopen("result.txt", "r+");
+    f3=fopen("resultMeuilleur.txt", "r+");
+    fseek(f1, 0, SEEK_END);
+    fseek(f2, 0, SEEK_END);
+    fseek(f3, 0, SEEK_END);
+    
     Node* tee = createTree();
     Node* teePire = createTree();
-    fillTree(tee, "rand.txt");
-    fillTree(teePire, "ascend.txt");
-    int x = rand();
-    
-    //
-    struct timespec start, end;
-    clock_gettime(CLOCK_REALTIME, &start);
+    fillTree(tee, "rand.txt", 10000);
+    fillTree(teePire, "ascend.txt", 10000);
+    for(int i=1; i<10000; i=i+5){
 
-    deleteNode(teePire, x); printf("\nDeleted\n");
+    
+    
+    
+    
+    //---------------------------------------//
+    clock_gettime(CLOCK_REALTIME, &start);
+    
+    deleteNode(teePire, 10000); 
+    //printf("\nDeleted\n");
+
+    clock_gettime(CLOCK_REALTIME, &end);
+    time_spent = (end.tv_sec - start.tv_sec) +
+                        (end.tv_nsec - start.tv_nsec) / BILLION;
+    sprintf(s, "%.10lf", time_spent);
+    sprintf(step, "%d", i);
+    strcat(step, " ");
+    strcat(step, s);
+    fputs(step, f1);
+    fputs("\n",f1);
+    //printf("The elapsed time is %.10lf seconds for the worst case.", time_spent);
+    //-------------------------------------------//
+
+    clock_gettime(CLOCK_REALTIME, &start);
+    
+    deleteNode(tee, 10000);
+    //printf("\nDeleted\n");
     
     clock_gettime(CLOCK_REALTIME, &end);
-    double time_spent = (end.tv_sec - start.tv_sec) +
+    time_spent = (end.tv_sec - start.tv_sec) +
                         (end.tv_nsec - start.tv_nsec) / BILLION;
-    printf("The elapsed time is %lf seconds for the worst case.", time_spent);
+    sprintf(s, "%.10lf", time_spent);
+    sprintf(step, "%d", i);
+    strcat(step, " ");
+    strcat(step, s);
+    fputs(step, f2);
+    fputs("\n",f2);
+       //printf("The elapsed time is %.10lf seconds for the random set (regular case)", time_spent);
+     //-------------------------------------------//
+    tee->left = newNode(-1);
 
-
+    //printTree(tee);
     clock_gettime(CLOCK_REALTIME, &start);
-    deleteNode(tee, x);
-    printf("\nDeleted\n");
+    
+    deleteNode(tee, -1);
+    
+    //printf("\nDeleted\n");
+    
     clock_gettime(CLOCK_REALTIME, &end);
-    double time_spent = (end.tv_sec - start.tv_sec) +
+    time_spent = (end.tv_sec - start.tv_sec) +
                         (end.tv_nsec - start.tv_nsec) / BILLION;
-        printf("The elapsed time is %lf seconds for the random set (regular case)", time_spent);
+    sprintf(s, "%.10lf", time_spent);
+    sprintf(step, "%d", i);
+    strcat(step, " ");
+    strcat(step, s);
+    fputs(step, f3);
+    fputs("\n",f3);
+        //printf("The elapsed time is %.10lf seconds for the best set (best case)", time_spent);
+
+    }
+    
+    //printf("\nlol : %d\n", tee->left->data);
+
+
+
+   
+
+
+     fclose(f1);
+     fclose(f2);
+     fclose(f3);
 
 
 
